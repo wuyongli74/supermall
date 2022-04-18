23c02ed178f0832c496f9bdb85c5dcf72be6dcf4
伍永丽 <yongliw@chinaautomarket.com>
2022-04-18 15:04:08 +0800

    首页轮播图，推荐，

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false,
+  configureWebpack: {
+    resolve: {
+      // 后缀名
+      extensions: [],
+      // 别名配置
+      alias: {
+        'assets': '@/assets',
+        'views': '@/views',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+      }
+    }
+  }
 }
